e1637d137e5111a1e0712fe73fc8bd1fea9b7e3f
uc-ssi <shubham.singh@ucertify.com>
2018-09-20 21:36:25 +0530

    Added PSQL database file

---
+++ smartbrainAPI/smart_brain.sql
@@ -0,0 +1,78 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               PostgreSQL 9.5.14, compiled by Visual C++ build 1800, 64-bit
+-- Server OS:                    
+-- HeidiSQL Version:             9.5.0.5196
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES  */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
